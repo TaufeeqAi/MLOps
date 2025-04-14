cacc088f890d94043838081e01f1c41f40650668
Taufeeque Ahmad <63660975+TaufeeqAi@users.noreply.github.com>
2025-04-14 17:11:15 +0530

    Data Transformation Stage completed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.MLOps import logger
 from src.MLOps.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.MLOps.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+from src.MLOps.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+
 
 STAGE_NAME= "Data Ingestion Stage"
 
@@ -15,12 +17,27 @@ except Exception as e:
         raise e
 
 
+
 STAGE_NAME= "Data Validation Stage"
 
 try:
     logger.info(f">>>>>> stage {STAGE_NAME}<<<<<<<")
     obj=DataValidationTrainingPipeline()
     obj.initiate_data_validation()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx=========x")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME= "Data Transformation Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME}<<<<<<<")
+    obj=DataTransformationTrainingPipeline()
+    obj.initiate_data_transformation()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx=========x")
 
 except Exception as e:
